6789d4155be5ec288f3e99973743260276d65028
Yuna012 <99405582+Yuna012@users.noreply.github.com>
2022-05-11 00:53:42 +0900

    commit

--- ProjectB/clock.js
+++ ProjectB/clock.js
@@ -1,52 +1,36 @@
 function setup() {
     createCanvas(1112, 834);
-background(0);
+background(255);
 }
 
-let hours, minutes, seconds;
+let hours, minutes;
 
 function draw() {
 clockFace();
-showTime(hour(), minute(), millis());
+showTime(hour(), minute());
 }
 
 function showTime(hours, minutes, millis){
-//hour hand
+//hour
 push();
 strokeWeight(7);
-stroke(0);
-rotate(radians(30 * hours + 0.5*minutes));
-line(0, 0, 0, 260);
+stroke("#261715");
+rotate(radians(minutes/60));
+line(0, 0, 0, 100);
 pop();
-//minute hand
+//minute
 push();
 strokeWeight(5);
-stroke(245, 0, 0);
-rotate(radians(6*minutes + 0.0001*millis));
-line(0, 0, 0, 310);
-pop();
-//second hand
-push();
-strokeWeight(3);
-stroke(0, 255, 0);
-rotate(radians(6 * 0.001 * millis));
-line(0, 0, 0, 350);
+stroke("#423204");
+rotate(radians(6*minutes));
+line(0, 0, 0, 135);
 pop();
 }
 
 function clockFace() {
 stroke(0);
-ellipse(width/2, height/2, height*.9, height*.9);
+ellipse(width/2, height/2, height*.4, height*.4);
 translate(width/2, height/2);
 rotate(PI);
-for (let i = 0; i < 12; i++) {
-    strokeWeight(3);
-    line(0, 310, 0, 350);
-    rotate(radians(30));
-}
-for (i = 0; i < 60; i++) {
-    strokeWeight(2);
-    line(0, 330, 0, 350);
-    rotate(radians(6));
-}
+
 }
