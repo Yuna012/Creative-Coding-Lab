ab68bcd6644c11f41deb47c61a438d1f5e5c6b11
Yuna012 <99405582+Yuna012@users.noreply.github.com>
2022-05-12 02:21:35 +0900

    commit

--- ProjectB/clock.js
+++ ProjectB/clock.js
@@ -1,35 +1,111 @@
 let seq = 0;
 let seqTime = 0;
+let yoff = 0.0;
+let x, y;
+let paint;
+let xSpd, ySpd;
+let dia;
 let hours, minutes;
+let a, b, c, d, e, f, g, h;
+
+function preload() {
+  a = loadFont('Macondo-Regular.ttf');
+  b = loadFont('Montserrat-Modern.ttf');
+  c = loadFont('Satisfy-Vintage.ttf');
+  d = loadFont('Acme-Jungle.ttf');
+  e = loadFont('DancingScript-CottonCandy.ttf');
+  f = loadFont('Cinzel-Office.ttf');
+  g = loadFont('PaytoneOne-NYU.ttf');
+  h = loadFont('Greek.ttf');
+  paint = loadImage('coconut-tree.png');
+}
 
 function setup() {
-    img = createImage(800, 800);
+  img = createCanvas(600, 600);
   background(255);
+  x = random(150, 250);
+  y = random(200, 250);
+  xSpd = random(-2, 2);
+  ySpd = random(-2, 2);
+  dia = random(80, 130);
 }
+// function mousePressed(){
+//   save(img, 'YourClock.png');
+// }
 
-function mousePressed() {
-    save(img, 'myImage.png');
-}
 function draw() {
   background(50);
   
     switch (seq) {
     case 0:
       drawIntro();
+  image(paint, -80, 315, 220, 350);
+  beginShape();
+  let xoff = 0;
+  fill('#7CF0EF');
+  noStroke();
+  for (let x = 0; x <= width; x += 15) {
+    let y = map(noise(xoff, yoff), 0, 1, 480, 580);
+    vertex(x, y);
+    xoff += 0.05;
+  }
+  yoff += 0.01;
+  vertex(width, height);
+  vertex(0, height);
+  endShape(CLOSE);
       break;
     case 1:
       drawScene1();
       break;
     case 2:
       drawScene2();
+    textFont(h);
+  textSize(30);
+  noStroke();
+  fill('black');
+  text('XII', width / 2.2, 180);
+  text('III', 500, 400);
+  text('IX', 70, 400);
       break;
     case 3:
       drawScene3();
+        noFill();
+        stroke('#0D2622');
+        strokeWeight(2);
+    arc(38, -10, 260, 170, 0, 40, OPEN);
+    arc(460, -10, 290, 115, 80, 130, OPEN);
+        strokeWeight(4);
+    arc(210, -30, 300, 200, 30, 180, OPEN);
+        stroke('#025920');
+        strokeWeight(5);
+    arc(45, -10, 200, 90, 0, 30, OPEN);
+    arc(330, -30, 200, 90, 0, 30, OPEN);
+    arc(510, -10, 240, 130, 0, 30, OPEN);
+        strokeWeight(8);
+    arc(240, 10, 360, 220, 50, 330, OPEN);
       break;
+      case 4: 
+        drawScene4();
+        break;
+        case 5:
+        drawScene5();
+        break;
     default:
       drawEnding();
       break;
   }
+  x = x + xSpd;
+
+  y = y + ySpd;
+
+  // change the direction
+  if (x < 0 || x > width) {
+    xSpd = xSpd * -1;
+  }
+  if (y < 0 || y > height) {
+    ySpd = ySpd * -1;
+  }
+  
   clock();
   showTime(hour(), minute());
 }
@@ -64,28 +140,12 @@ function keyPressed() {
 
 function proceedSequence() {
   seq++;
-  if (seq == 5) {
+  if (seq == 7) {
     // if sequence reached the last phase,
     // we reset the sequence.
     seq = 0;
   }
 }
-// function updateSequence() {
-//   if (seqTime < 100) {
-//     seq = 0;
-//   } else if (seqTime > 100 && seqTime < 200) {
-//     seq = 1;
-//   } else if (seqTime > 200 && seqTime < 300) {
-//     seq = 2;
-//   } else if (seqTime > 300 && seqTime < 400) {
-//     seq = 3;
-//   } else if (seqTime > 400 && seqTime < 500) {
-//     seq = 4;
-//   } else {
-//     seq = 5;
-//   }
-//   seqTime++;
-// }
 
 function drawIntro() {
 //theme tropical
@@ -93,26 +153,44 @@ function drawIntro() {
   fill("#FFC900");
   stroke("#9BE800");
   strokeWeight(7);
-  ellipse(width / 2, height / 2, 300, 300);
+  textFont(a);
+  textSize(50);
+  text('Tropical', width / 2.75, 60);
+  ellipse(width / 2, height / 2, 250, 250);
 }
 
 function drawScene1() {
 //modern
   background("#F2A027");
   fill("#E8E8E6");
+  textFont(b);
+  textSize(50);
+  text('Modern', width / 2.9, 80);
+  noFill();
+  line(100, 0, 100, 100);
+  fill('#FFD528AF');
+  noStroke();
+  triangle(95, 75, 500, 600, -250, 600);
+  fill('#272F32');
+  arc(100, 105, 100, 80, PI, TWO_PI);
+  fill("#E8E8E6");
   stroke("#272F32");
-  strokeWeight(2);
-  rectMode(CENTER);
-  rect(width / 2, height / 2, 220, 200);
+  strokeWeight(4);
+  triangle(120, 420, 300, 150, 480, 420);
 }
 
 function drawScene2() {
 //vintage
   background("#735236");
+  fill("#F0DAC0");
+  stroke("#886B52");
+  strokeWeight(8);
+  textFont(f);
+  textSize(50);
+  text('Vintage', width / 3.2, 80);
+  strokeWeight(10);
   fill("#A69580");
-  stroke("#A68263");
-  strokeWeight(4);
-  ellipse(width / 2, height / 2, 310, 170);
+  arc(width / 2, height / 1.52, 360, 400, PI, TWO_PI);
 }
 
 function drawScene3() {
@@ -121,17 +199,29 @@ function drawScene3() {
   fill("#365902");
   stroke("#022601");
   strokeWeight(8);
+  textFont(d);
+  textSize(50);
+  text('Jungle', width / 2.5, 80);
   rectMode(CENTER);
-  rect(width / 2, height / 2, 200, 200);
+  rect(width / 2, height / 2, 220, 220);
 }
 
 function drawScene4(){
 //cotton candy
   background("#C099F1");
-  fill("#BFE3F3");
   stroke("#F5DFB5");
   strokeWeight(3);
-  ellipse(width / 2, height / 2, 300, 300);
+  textFont(e);
+  textSize(50);
+  text('Cotton Candy', width / 3.25, 100);
+  noFill();
+  ellipse(x / 3, y, dia);
+  fill("#BFE3F3");
+  ellipse(width / 2, height / 2, 260, 260);
+  noFill();
+  ellipse(x + 50, y / 2, 160);
+  noFill();
+  ellipse(x / 2, y + 100, 30);
 }
 
 function drawScene5(){
@@ -139,9 +229,21 @@ function drawScene5(){
   background("#EEF277");
   fill("#D9D9D9");
   stroke("#262626");
-  strokeWeight(14);
+  strokeWeight(5);
+  textFont(f);
+  textSize(50);
+  text('Office', width / 2.9, 90);
   rectMode(CENTER);
-  rect(width / 2, height / 2, 230, 230);
+  strokeWeight(7);
+  polygon(width / 2, height / 2, 130, 7);
+  fill('#99794E');
+  noStroke();
+  rect(300, 600, 600, 200);
+  fill('black');
+  rect(520, 500, 5, 15);
+  rect(520, 510, 80, 2);
+  fill('grey');
+  rect(520, 450, 149.8, 89.8);
 }
 
 function drawEnding() {
@@ -150,5 +252,33 @@ function drawEnding() {
   fill("#DAC2F2");
   stroke("#340B53");
   strokeWeight(5);
-  ellipse(width / 2, height / 2, 250, 300);
+  textFont(g);
+  textSize(100);
+  text('NYU', width / 3.2, 100);
+  star(width / 2, height / 2, 100, 180, 5);
+}
+
+function star(x, y, radius1, radius2, npoints) {
+  let angle = TWO_PI / npoints;
+  let halfAngle = angle / 2.0;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a + 50) * radius2;
+    let sy = y + sin(a + 50) * radius2;
+    vertex(sx, sy);
+    sx = x + cos(a + halfAngle + 50) * radius1;
+    sy = y + sin(a + halfAngle + 50) * radius1;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
+function polygon(x, y, radius, npoints) {
+  let angle = TWO_PI / npoints;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius;
+    let sy = y + sin(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
 }
